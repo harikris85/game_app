467b0fc3037c04515f2e0a188198a99f3a3b47a3
harikrishnan <harikc1985@gmail.com>
2014-11-28 23:39:32 -0800

    Added gmaps and related files

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+  	@all_games = Game.all
+  	if ((cookies[:sport]).nil?) 
+  		#@game = @all_games.where(sport: "Football")
+      @game = @all_games
+  	else
+  		@game = @all_games.where(sport: cookies[:sport])
+  	end
+	@hash = Gmaps4rails.build_markers(@game) do |game, marker|
+  	marker.lat game.latitude
+  	marker.lng game.longitude
+    marker.infowindow render_to_string(:partial => 'games/show', :layout => false, :locals => { :@game => game })   
+    end
   end
 
   def help
