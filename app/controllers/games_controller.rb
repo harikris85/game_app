3a6fba490585c0014e1e0d8bc022c637885410ec
harikrishnan <harikc1985@gmail.com>
2014-12-13 17:32:25 -0800

    Games destroy added, game_time added, total_gamers added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,22 @@
 class GamesController < ApplicationController
-  before_action :signed_in_user
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
   respond_to :json, :html
 
   def create
   	@game = current_user.games.build(game_params)
     if @game.save
       flash[:success] = "Game created!"
-      redirect_to root_url
+      redirect_to current_user
     else
       render 'static_pages/home'
     end
   end
 
   def destroy
+    @game.destroy
+    flash[:success] = "Sport deleted"
+    redirect_to current_user
   end
 
   def show
@@ -21,7 +25,12 @@ class GamesController < ApplicationController
   private
 
     def game_params
-      params.require(:game).permit(:address, :longitude, :latitude, :description, :sport)
+      params.require(:game).permit(:address, :longitude, :latitude, :description, :sport, :game_time)
+    end
+
+     def correct_user
+      @game = current_user.games.find_by(id: params[:id])
+      redirect_to root_url if @game.nil?
     end
 
 end
