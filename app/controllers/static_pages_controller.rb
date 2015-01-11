cd7e592ee8c9c7a77d9409cefd0835ba3a134fb1
harikrishnan <harikc1985@gmail.com>
2015-01-11 00:30:18 -0800

    New games and change in color

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,10 +16,46 @@ class StaticPagesController < ApplicationController
       image = "/assets/soccer.png"
     elsif (game.sport == "Cricket")
       image = "/assets/cricket.png"
-    elsif (game.sport == "Basketball")
+    elsif (game.sport == "Basket ball")
       image = "/assets/basketball.png"
-    elsif (game.sport == "Volleyball")
+    elsif (game.sport == "Volley ball")
       image = "/assets/volleyball.png"
+    elsif (game.sport == "Badminton")
+      image = "/assets/badminton.png"
+    elsif (game.sport == "Baseball")
+      image = "/assets/baseball.png"
+    elsif (game.sport == "Billard")
+      image = "/assets/billard.png"
+    elsif (game.sport == "Chess")
+      image = "/assets/chess.png"
+    elsif (game.sport == "Climbing")
+      image = "/assets/climbing.png"
+    elsif (game.sport == "Cycling")
+      image = "/assets/cycling.png"
+    elsif (game.sport == "Fitness")
+      image = "/assets/fitness.png"
+    elsif (game.sport == "Golfing")
+      image = "/assets/golfing.png"
+    elsif (game.sport == "Hangout")
+      image = "/assets/hangout.png"
+    elsif (game.sport == "Hiking")
+      image = "/assets/hiking.png"
+    elsif (game.sport == "Jogging")
+      image = "/assets/jogging.png"
+    elsif (game.sport == "Motorbike")
+      image = "/assets/motorbike.png"
+    elsif (game.sport == "Squash")
+      image = "/assets/squash.png"
+    elsif (game.sport == "Swimming")
+      image = "/assets/swimming.png"    
+    elsif (game.sport == "Table tennis")
+      image = "/assets/tabletennis.png" 
+    elsif (game.sport == "Tennis")
+      image = "/assets/tennis.png"                           
+    elsif (game.sport == "US football")
+      image = "/assets/usfootball.png" 
+    elsif (game.sport == "Yoga")
+      image = "/assets/yoga.png"       
     end
 
   	marker.picture({
